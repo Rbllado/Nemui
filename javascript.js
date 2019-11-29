84656875ff8b4f1140985303555acd192bda3109
Ivan <ifrancesalcantara@gmail.com>
2019-11-29 10:40:26 +0100

    name and img in search view

--- javascript.js
+++ javascript.js
@@ -1,17 +1,35 @@
 const searchByIdBaseUrl= "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-const searchByIgredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin"
+const searchByIgredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
 const button = document.querySelector("#get")
 const input = document.querySelector("#input")
 const form = document.querySelector("#get-form")
-
+const allCocktails = document.querySelector("#allCocktails")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
     if(!input.value) return;
+    console.log(searchByIgredient+input.value);
+    
     axios.get(searchByIgredient+input.value)
     .then(function (response) {
         // handle success
-        console.log(response);
+        console.log(response.data.drinks);
+        response.data.drinks.forEach(drink=>{
+            console.log("drink");
+            
+            const drinkDiv = document.createElement("div")
+            const drinkTitle = document.createElement("h3")
+            const drinkImg = document.createElement("img")
+
+            drinkTitle.innerHTML = drink.strDrink
+            drinkImg.setAttribute("src", drink.strDrinkThumb)
+
+            drinkDiv.appendChild(drinkTitle)
+            drinkDiv.appendChild(drinkImg)
+
+
+            allCocktails.appendChild(drinkDiv)
+        })
       })
       .catch(function (error) {
         // handle error
